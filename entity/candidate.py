2dc28abb575d212d7b56dc39e3578e8350d249c3
supuli-obeysekera <160041480+supuli-obeysekera@users.noreply.github.com>
2024-12-04 09:54:48 +0530

    committing class candidate

---
+++ entity/candidate.py
@@ -0,0 +1,8 @@
+import sqlite3 as db
+
+class Candidate:
+    def __init__(self, candidate_id = ,  candidate_name = "", candidateHouse=""):
+        self.candidate_id = candidate_id
+        self.candidate_name = candidate_name
+        self.candidateHouse = candidateHouse
+
