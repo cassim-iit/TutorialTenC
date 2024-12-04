2d1e8a79a325a20b64938f83aa599b1c275f94d9
supuli-obeysekera <160041480+supuli-obeysekera@users.noreply.github.com>
2024-12-04 10:02:17 +0530

    committing insert candidate in the candidate class

--- entity/candidate.py
+++ entity/candidate.py
@@ -1,8 +1,16 @@
 import sqlite3 as db
 
 class Candidate:
-    def __init__(self, candidate_id = ,  candidate_name = "", candidateHouse=""):
+    def __init__(self, candidate_id = 0,  candidate_name = "", candidateHouse=""):
         self.candidate_id = candidate_id
         self.candidate_name = candidate_name
         self.candidateHouse = candidateHouse
 
+    def save(self):
+        sql = "INSERT INTO candidate (candidateID, candidateName, candidateHouse) VALUES (?, ?, ?)"
+        con = db.connect("database\\candidate.db")
+        cur = con.cursor()
+        cur.execute(sql, (self.candidate_id, self.candidate_name,self.candidateHouse))
+        con.commit()
+        cur.close()
+        con.close()
